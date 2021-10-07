44aa9f73cfbf09b8839aded3d62419ed677d0432
Kory Attleson <kory.attleson@gmail.com>
2021-10-07 11:49:52 -0500

    Create authentication app urls file + Register auth app URLS in project URLS file.

--- gymtracker/authentication/serializers.py
+++ gymtracker/authentication/serializers.py
@@ -1,6 +1,6 @@
-from typing_extensions import Required
+# from typing_extensions import Required
 from rest_framework import serializers 
-from django.auth.models import User
+from django.contrib.auth.models import User
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
 
