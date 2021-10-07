9c0d96699354922eab72c187b50905c5dd67acdc
Kory Attleson <kory.attleson@gmail.com>
2021-10-07 13:10:45 -0500

    Updated ERD, Updated Gym App + Created Gym Model + REgistered Gym model with admin site + migrated new car table.

--- gymtracker/gym/models.py
+++ gymtracker/gym/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Gym(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    exercise = models.CharField(max_length=45)
+    sets = models.IntegerField(default=0)
+    reps = models.IntegerField(default=0)
+    weight = models.IntegerField(default=0)
