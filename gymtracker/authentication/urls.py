d62acf283c31da89c7c69b69e972444de570ed05
Kory Attleson <kory.attleson@gmail.com>
2021-10-11 08:52:39 -0500

    Attempting to create a logout function in autentication App, adjustments in urls.py and views.py

--- gymtracker/authentication/urls.py
+++ gymtracker/authentication/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from rest_framework import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterView
+from . import views
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_error'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('logout/', views.logout_request, name='logout')
 ]
--- gymtracker/authentication/views.py
+++ gymtracker/authentication/views.py
@@ -2,9 +2,13 @@ from django.contrib.auth.models import User
 from .serializers import RegistrationSerializer
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from django.contrib.auth import logout
 # Create your views here.
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegistrationSerializer
+
+def logout_request(request):
+    logout(request)
